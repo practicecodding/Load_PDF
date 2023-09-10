870e1c0a8d2b494449ebe06a1a101a73903c024c
user <practicecoddin@gmail.com>
2023-09-10 07:41:24 +0600

    Load PDF

---
+++ app/src/main/java/com/hamidul/loadpdf/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hamidul.loadpdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+    PDFView pdfView;
+    LottieAnimationView animationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pdfView = findViewById(R.id.pdfView);
+        animationView = findViewById(R.id.animationView);
+
+
+        pdfView.fromAsset("cv_ujjal.pdf")
+                .onLoad(new OnLoadCompleteListener() {
+                    @Override
+                    public void loadComplete(int nbPages) {
+                        pdfView.setVisibility(View.VISIBLE);
+                        animationView.setVisibility(View.GONE);
+                    }
+                })
+                .load();
+
+    }
+}
